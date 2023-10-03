aeeedf7db9c89034848e813458e2ba074afbb960
garciazapiain <juangarciazapiain@gmail.com>
2023-10-03 09:45:27 +0200

    build vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Change this to 'build'
+  },
   plugins: [react()],
 })
