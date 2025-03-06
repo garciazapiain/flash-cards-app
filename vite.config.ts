81b23c918b54a958e313eee9b7aaf0adbd55d2f9
garciazapiain <juangarciazapiain@gmail.com>
2025-03-06 10:22:30 -0600

    config correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  build: {
+    outDir: 'build', // Change this to 'build'
+  },
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: "My PWA App",
-        short_name: "PWA",
-        description: "A Progressive Web App",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-      },
-    }),
   ],
 });
